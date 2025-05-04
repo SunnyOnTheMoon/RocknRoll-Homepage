6434dc7c58afa5984573034db436ac183e828772
Sunny <reeb.dennis@gmail.com>
2025-05-04 13:43:38 +0200

    Navigation responsive designed

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
     selector: 'app-navigation',
@@ -9,6 +11,8 @@ import { RouterModule } from '@angular/router';
         RouterModule,
         MatToolbarModule,
         MatButtonModule,
+        MatSidenavModule,
+        MatListModule 
     ],
     templateUrl: './navigation.component.html',
     styleUrl: './navigation.component.scss'
