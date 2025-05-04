44a83d5a2234742157e69068ffdcc9534df1ef3a
Sunny <reeb.dennis@gmail.com>
2025-05-04 10:11:26 +0200

    Impressum added

--- src/app/pages/auftritte/auftritte.component.ts
+++ src/app/pages/auftritte/auftritte.component.ts
@@ -12,7 +12,7 @@ import { VideoPlayerComponent } from "../../components/video-player/video-player
     styleUrl: './auftritte.component.scss'
 })
 export class AuftritteComponent {
-     // Auftritt 1
+
   public auftritt1 = {
     title: '04.11.2023: Der etwas andere Showtanzabend',
     description: `Nachdem die Teilnahme 2022 krankheitsbedingt abgesagt werden musste, war es nun endlich soweit: 
@@ -20,15 +20,14 @@ export class AuftritteComponent {
                   Zum ersten Mal betraten dabei zwei getrennte Formationen die Bühne: Zunächst trat der F-Kurs mit der neuen 
                   "Tick-Tock"-Choreo auf, nach der Pause dann der L-Kurs mit der Elvis-Choreo. Leider war die Beleuchtung so stark, 
                   dass die Identifizierung der einzelnen Paare nur dem geübten Auge möglich sein sollte. Viel Spaß dabei. ;-)`,
-    videoId: 'uHAVIAKw4b8' // Beispiel für Video-ID (YouTube)
+    videoId: 'uHAVIAKw4b8' 
   };
 
-  // Auftritt 2
   public auftritt2 = {
     title: '02.11.2023: Rock\'n\'Roll Show',
     description: `Ein aufregender Auftritt beim Rock'n'Roll Turnier in Mainz! Es war ein tolles Event, bei dem wir alle unser Bestes gegeben haben.
                   Die Zuschauer waren begeistert und das Team hat einen großartigen Job gemacht. Schaut euch das Video an!`,
-    videoId: '9uQ0mvQQ7pc' // Beispiel für Video-ID (YouTube)
+    videoId: '9uQ0mvQQ7pc'
   };
 
   // Weitere Auftritte können hier hinzugefügt werden
